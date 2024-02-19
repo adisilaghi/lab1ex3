b5944a4fea3a7fe58d38db19eb6ac368a8ed319b
Administrator <Administrator@DESKTOP-IKD2D8I>
2024-02-19 20:58:48 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura*/
+
+Console.WriteLine("Introduceti numarul");
+int Numar = Convert.ToInt32(Console.ReadLine());
+int Ultima_cifra = Numar % 10;
+Console.WriteLine("Ultima cifra este "+Ultima_cifra);
